97947f8bed5bb9a986b2bf25a1675c60c5cf668a
russ1111 <rschneider77@gmail.com>
2016-10-24 16:09:26 -0700

    testing

--- about.php
+++ about.php
@@ -13,7 +13,6 @@
 		<h2><a href="<?php the_permalink();//link to the page or posting?>"><?php the_title();//get the page or posting ?> </a></h2>
 		<?php the_content(''); //get the page or posting written content ?>
 		<?php endwhile; endif; //end the loop?>
-		<small>page.php</small>
 		</div>
     <!--End Text-->
             
