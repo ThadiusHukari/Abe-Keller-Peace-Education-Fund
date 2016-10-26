3a32aa4d2a1d394e25fcc16cc6b07b080fe295b8
Tatiana Zaccara <tazaccara@gmail.com>
2016-10-25 20:13:42 -0700

    Remove "Latest Posting"from home page

--- front-page.php
+++ front-page.php
@@ -19,17 +19,6 @@
 	    <?php endwhile; endif; // end loop one ?>
     </section>
     
-    <section class="widget-item">
-	<h1>Latest Postings</h1>
-	<ul>
-	    <!--Loop Two-->
-	    <?php rewind_posts(); //stop loop one ?>
-	    <?php query_posts('showposts=4')//give directions to loop two ?>
-	    <?php while( have_posts() ) : the_post(); // start loop two ?>
-	    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-	    <?php endwhile; //end loop two ?> 
-	</ul>
-    </section>
     
     <section class="widget-item">
     </section>
